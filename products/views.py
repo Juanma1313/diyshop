1b8bd934b841a15394e5d3e47a86eca1903264bf
Juanma1313 <j.heras13@gmail.com>
2024-05-16 04:23:57 +0200

    added js changes for toasts on bootstrap V5, CSS and custom shopping bag toast messages

--- products/views.py
+++ products/views.py
@@ -66,6 +66,7 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
+                print("ERR: No search criteria!")
                 messages.error(request, ("You didn't enter any search criteria!"))
                 return redirect(reverse('products'))
 
