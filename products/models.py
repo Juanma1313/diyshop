147ddd6028abf3ce3733375ed166b1daf94463a9
Juanma1313 <j.heras13@gmail.com>
2024-05-22 09:28:16 +0200

    added edit_product/delete_product functionality and security

--- products/models.py
+++ products/models.py
@@ -29,7 +29,7 @@ class Thing(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
     featured_image_url = models.URLField(max_length=1024, null=True, blank=True)
-    featured_image = models.ImageField(null=True, blank=True)
+    featured_image = models.ImageField(null=True, blank=True, default='noimage.png')
     description = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
--- products/views.py
+++ products/views.py
@@ -13,7 +13,10 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
     #products = Thing.objects.filter(status=1, parent__isnull=True).order_by('-created_on')
     # Select all the publiched products that are root or have instructions
-    products = Thing.objects.filter(Q(status=1, parent__isnull=True) | Q(status=1, instructions__isnull=False) ).distinct()
+    if request.user.is_superuser or request.user.is_staff:
+        products = Thing.objects.filter(Q(parent__isnull=True) | Q(instructions__isnull=False) ).distinct()
+    else:
+        products = Thing.objects.filter(Q(status=1, parent__isnull=True) | Q(status=1, instructions__isnull=False) ).distinct()
 
     query = None
     categories = None
@@ -124,8 +127,8 @@ def productlike(request, product_id):
 @login_required
 def add_product(request):
     """ Add a product to the store """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+    if not request.user.is_superuser and not request.user.is_staff:
+        messages.error(request, 'Sorry, unauthorized operation.')
         return redirect(reverse('home'))
 
     if request.method == 'POST':
@@ -147,3 +150,45 @@ def add_product(request):
     }
 
     return render(request, template, context)
+@login_required
+def edit_product(request, product_id):
+    """ Edit a DIY Project in the store """
+    if not request.user.is_superuser and not request.user.is_staff:
+        messages.error(request, 'Sorry, unauthorized operation.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Thing, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated DIY Project!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request,
+                           ('Failed to update DIY Project. '
+                            'Please ensure the information is valid.'))
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.title}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    """ Delete a DIY Project  from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, unauthorized operation.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Thing, pk=product_id)
+    product.delete()
+    messages.success(request, 'DIY Project deleted!')
+    return redirect(reverse('products'))
